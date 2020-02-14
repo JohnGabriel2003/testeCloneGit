0340adbfa550d65d469f71a06f194a393d8e400d
brunloyola02 <brunloyola02@gmail.com>
2020-02-14 08:28:00 -0300

    exemplo

--- teste.c
+++ teste.c
@@ -2,7 +2,15 @@
 #include <stdio.h>
 
 int main(){
+	int idade;
 
+	printf("Idade: ");
+	scanf("%i", &idade);
+
+	idade = idade - 1;
+
+	printf("Idade - 1 = %i", idade);
+	printf("\n");
 
     return 0;
 }
